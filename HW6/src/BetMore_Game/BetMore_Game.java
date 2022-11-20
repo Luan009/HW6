45e34c08fdbabf235d915cfd0f57808cf68d1334
lzekiri <lzekiri@smail.uni-koeln.de>
2022-11-20 23:05:01 +0100

    Update

--- HW6/src/BetMore_Game/BetMore_Game.java
+++ HW6/src/BetMore_Game/BetMore_Game.java
@@ -18,60 +18,7 @@ public class BetMore_Game {
 		
 		x = s.pick_card();
 		
-		System.out.println("Your number: " + x);
-		
-		System.out.println("Bet on this number? 1 for yes, 2 for no");
-		
-		Scanner a = new Scanner(System.in);
-		
-		int t = a.nextInt();
-		
-		
-		if(t == 1) {
-			user_Selected = true;
-		} else {
-			user_Selected = false;
-		}
-		
-		
-		while(user_Selected == false) {
-			
-			for(int i = 0; i < 5; i++) {
 				
-				x = s.pick_card();
-				
-				System.out.println("Your new number: " + this.x);
-				
-				if(i == 4 && user_Selected == false) {
-					System.out.println("Can't choose new number.");
-					user_Selected = true;
-					break;
-				}
-				
-				Scanner b = new Scanner(System.in);
-				System.out.println("Bet on this number? 1 for yes, 2 for no");
-				
-				int t1 = b.nextInt();
-				
-				
-				
-				if(t1 == 1) {
-					user_Selected = true;
-					break;
-					
-				} else {
-					user_Selected = false;
-					continue;
-				}
-				
-				
-			}
-		
-			
-		}
-		
-		System.out.println("Your chosen number: " + x);
-		
 		return x;
 		
 	}
--- HW6/src/BetMore_Game/Game_Chair.java
+++ HW6/src/BetMore_Game/Game_Chair.java
@@ -2,21 +2,37 @@ package BetMore_Game;
 
 public class Game_Chair {
 	
+	static BetMore_Game Bet = new BetMore_Game();
+	static Player Computer = new Player();
 	
-	
-	public Player Computer = new Player();
-	
-	public Player Player_2 = new Player();
-	
-	
-	BetMore_Game Bet = new BetMore_Game();
+	static Player Player_2 = new Player();
 	
 	
 	
 	
+	public static void main(String[] args) {
+		
+		
+		Computer.user_Selected = true;
+		Player_2.user_Selected = false;
+		
+		Computer.Start();
+
+		Player_2.Start();
+		
+		
+		
+		
+		System.out.println(result());
+		
+		
+	}
 	
 	
-	public String result(Player Computer, Player Player_2) {
+	
+	
+	
+	public static String result() {
 		
 		return Bet.FindWinner(Computer, Player_2);
 		
